58763ee06c147fbaa7e19c8b8069b3c407928845
hyemin386 <ha_ha24@naver.com>
2021-05-10 22:55:24 +0900

    1

---
+++ src/main/java/com/hm/j1/SpringBootMyBatisJspApplication.java
@@ -0,0 +1,13 @@
+package com.hm.j1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyBatisJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyBatisJspApplication.class, args);
+	}
+
+}

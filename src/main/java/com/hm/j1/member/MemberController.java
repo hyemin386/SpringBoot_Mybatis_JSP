6d9787a282e9e3725961ba8218fdcb165d22f9f0
hyemin386 <ha_ha24@naver.com>
2021-05-24 09:19:32 +0900

    commit

--- src/main/java/com/hm/j1/member/MemberController.java
+++ src/main/java/com/hm/j1/member/MemberController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ public class MemberController {
 	}
 	
 	@PostMapping("join")
-	public String join(@Valid MemberVO memberVO, BindingResult bindingResult) throws Exception { //Valid 검증하겠다는 뜻
+	public String join(@Valid MemberVO memberVO, Errors bindingResult) throws Exception { //Valid 검증하겠다는 뜻
 		//bindingResult에 검증된 결과를 담음
 		if(bindingResult.hasErrors()) { //검증 시 에러가 발생하면 join으로
 			return "member/join";
